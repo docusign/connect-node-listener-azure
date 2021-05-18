5a8459d4683c78afdee6f5c6b212d8b8978034ac
Nima <nima.poulad@docusign.com>
2021-05-18 15:40:28 -0700

    Add support for JSON messages as well as XML

--- ConnectMessage/postSetup.sh
+++ ConnectMessage/postSetup.sh
@@ -7,5 +7,5 @@ echo "What is your FunctionApp name?"
 read functionApp
 
 az deployment group create --name sdDeployment --resource-group $rg --template-file template.json
-connectionstr=$(az servicebus namespace authorization-rule keys list --resource-group $rg --namespace-name Connect-Events-DS --name RootManageSharedAccessKey --query primaryConnectionString --output tsv)
+connectionstr=$(az servicebus namespace authorization-rule keys list --resource-group $rg --namespace-name Example-Connect-Events-DS --name RootManageSharedAccessKey --query primaryConnectionString --output tsv)
 az functionapp config appsettings set --name $functionApp --resource-group $rg --settings "SVC_BUS_CONNECTION_STRING=$connectionstr"  
